e1e01b06be589aefbf128be46c4458d4ad0c975c
Said Masoud <saidmasoud2005@gmail.com>
2017-11-10 22:00:24 -0500

    Basic error/case handling

--- src/s3.py
+++ src/s3.py
@@ -1,31 +1,37 @@
 import boto3
-'''
-TODO: 
-    -Create bucket if it doesn't exist
-    -Write model to bucket
+from botocore.exceptions import ClientError
 
+# TODO: write some docs for each function
 
-'''
 class S3:
 
     def __init__(self,bucket_name):
         self.s3 = boto3.client('s3')
-        self.name = bucket_name
+        self.bucket_name = bucket_name
 
-    
-    def create():
+    def create(self):
         response = self.s3.list_buckets()
-        if self.bucket_name in response['Buckets']:
-            print("Bucket {} already exists".format(self.bucket_name))
-        else:
-            try:
-                response = self.s3.create_bucket(Bucket=self.bucket_name)
-            except ClientError:
-                raise ValueError("Failed to create bucket. Try a different bucket name.")
+        # We have to extract the 'Buckets' object, which contains objects for each bucket.
+        for bucket in response['Buckets']:
+            if self.bucket_name == bucket['Name']:
+                raise ValueError("A bucket named '{}' already exists under your current AWS account.".format(self.bucket_name))
+            else:
+                try:
+                    response = self.s3.create_bucket(Bucket=self.bucket_name,
+                            # TODO: Need to figure out how to make the region configurable.
+                            CreateBucketConfiguration={'LocationConstraint': 'us-east-2'},)
+                except ClientError as e:
+                    if e.response['Error']['Code'] == 'BucketAlreadyExists':
+                        raise ValueError("Failed to create bucket '{}': already exists under separate AWS account. Try a different bucket name.".format(self.bucket_name))
+                    else:
+                        print(e)
     
     def write_model(self,model,model_name):
-        response = self.s3.get_object(Bucket=self.bucket_name,Key=model_name)
-        if response['Body']:
+        try:
+            response = self.s3.get_object(Bucket=self.bucket_name,Key=model_name)
             raise ValueError("A model with the specified name already exists")
-        else:
-            self.s3.put_object(Bucket=self.bucket_name,Key=model_name,Body=model)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                #This is a good sign: it means the model hasn't been pushed to S3 yet.
+                self.s3.put_object(Bucket=self.bucket_name,Key=model_name,Body=model)
+
